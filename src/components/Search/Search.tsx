c456425dcbc4ed5e70c802422ed098f66e39b5e5
SrAbnerP <abner.sol.pereira@gmail.com>
2024-02-07 11:28:09 -0300

    alterações finais no projeto

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,3 @@
-import { IoIosSearch } from "react-icons/io";
-
 import "./Search.scss";
 
 function Search() {
@@ -12,7 +10,28 @@ function Search() {
       />
 
       <button type="submit" className="search-button">
-        <IoIosSearch />
+        <svg
+          width="28"
+          height="28"
+          viewBox="0 0 28 28"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12.6875 21.875C17.7616 21.875 21.875 17.7616 21.875 12.6875C21.875 7.61338 17.7616 3.5 12.6875 3.5C7.61338 3.5 3.5 7.61338 3.5 12.6875C3.5 17.7616 7.61338 21.875 12.6875 21.875Z"
+            stroke="#9F9F9F"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M19.1837 19.1843L24.4994 24.5"
+            stroke="#9F9F9F"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
       </button>
     </form>
   );
